28529849e10b0d76fa8245d3b79b2694a731c134
Luke <luke.e.philips@gmail.com>
2017-02-24 14:44:13 -0800

    integration test through jobs#show

--- spec/features/worker_pages_spec.rb
+++ spec/features/worker_pages_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe "the create a job path" do
   it 'logs in and claims a job' do
   worker = FactoryGirl.create(:worker)
+  user = FactoryGirl.create(:user)
+  job = FactoryGirl.create(:job)
   visit ''
   click_link 'Log In as Worker'
   fill_in 'Email', :with => 'worker@thing.com'
@@ -10,5 +12,14 @@ describe "the create a job path" do
   expect(page).to have_content "Password"
   click_on 'Log in'
   expect(page).to have_content "You're signed into your worker@thing.com worker account"
+
+  click_on 'See All Jobs'
+  expect(page).to have_content job.title
+
+  click_link job.title
+  expect(page).to have_content job.description
+
+
+
   end
 end
